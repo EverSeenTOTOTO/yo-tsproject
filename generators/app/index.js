2663a19dbb6fab69bbb115b81f00de845968e9e6
EverSeenTOTOTO <everseenflash@163.com>
2021-07-04 14:06:29 +0800

    fix bugs

--- generators/app/index.js
+++ generators/app/index.js
@@ -29,33 +29,33 @@ module.exports = class extends Generator {
   writing() {
     const { name, target } = this.answers;
     if(this.existsDestination(name)) {
-      this.log.error(`${name} folder is not empty`);
-      process.exit(-1)
+      this.log.error(`${name} folder already exists`);
+      process.exit(1);
     }
 
     try {
       this.spawnCommandSync("mkdir", [ name ]);
     }catch(e) {
       this.log.error(e.message);
-      console.error(e);
-      process.exit(-1);
+      process.exit(1);
     }
 
-    const target = this.answers.target;
+    this.destinationRoot(name);
+
     if(['nodejs', 'web'].indexOf(target) === -1) {
       this.log.error('invalid target: ', target);
-      process.exit(-1)
+      process.exit(1);
     }
 
     const pkg = pkgJson[target];
     pkg.name = name;
 
-    this.fs.extendJSON(this.destinationPath('package.json'), pkg);
 
     this.fs.copyTpl(
       this.templatePath(`./${target}`),
       this.destinationPath(`./`),
-    );
+      );
+    this.fs.extendJSON(this.destinationPath('package.json'), pkg);
 
     [
       '.env',
